4f57f0437e9e537e696e59399ac96117b8ed5291
kouki920 <74360349+kouki920@users.noreply.github.com>
2021-01-19 23:45:01 +0900

    Add files via upload

---
+++ db/dbconnect.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ .'/../../vendor/autoload.php';
+
+
+function dbConnect(){
+
+    try {
+            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+
+
+                     $host = $_ENV['DB_HOST'];
+                     $user = $_ENV['DB_USER'];
+                     $pass = $_ENV['DB_PASS'];
+                     $dbname = $_ENV['DB_NAME'];
+
+                    $dsn = "mysql:host=${host};dbname=${dbname};charset=utf8mb4";
+
+                    $dbh = new PDO($dsn,$user,$pass,[PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>false,]);
+                    echo '接続成功';
+
+
+    } catch (PDOException $e) {
+            echo '接続失敗' . $e->getMessage() .  "\n";
+            exit();
+    }
+    return $dbh;
+}
+
+
+
+
+
+
+?>
