05cb6449ac70bef1d4f6d9341494da27522751e2
kouki920 <74360349+kouki920@users.noreply.github.com>
2021-01-21 17:55:49 +0900

    Update index.php

--- view/index.php
+++ view/index.php
@@ -1,14 +1,22 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php ?>
-    <ul>
-        <li></li>
-    </ul>
-</body>
-</html>
+
+    <?php if(count($memos)) :?>
+<ul>
+<?php foreach($memos as $memo) : ?>
+<li><?php echo htmlspecialchars($memo['memo'],ENT_QUOTES,'UTF-8') ;?></li>
+<li style="list-style: none;"><?php echo $memo['created_at'];?></li>
+
+<form action="../app/delete.php" method="POST">
+<input type="hidden" name="id" value="<?php echo $memo['id'];?>">
+<input type="submit" name='submit' value="削除">
+</form>
+
+<form action="../app/view/update.php" method="POST">
+<input type="hidden" name="id" value="<?php echo $memo['id'];?>">
+<input type="hidden" name="memo" value="<?php echo $memo['memo']?>">
+<input type="submit" name="submit" value="編集">
+</form>
+
+<hr>
+<?php endforeach;?>
+</ul>
+<?php endif;?>
