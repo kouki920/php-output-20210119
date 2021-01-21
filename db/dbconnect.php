7fee499a7035c095d2bb2414d45314e9fbd2bcfc
kouki920 <74360349+kouki920@users.noreply.github.com>
2021-01-21 17:54:30 +0900

    Update dbconnect.php

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -2,33 +2,37 @@
 
 declare(strict_types=1);
 
+
 require_once __DIR__ .'/../../vendor/autoload.php';
 
+ function dbConnect(){
 
-function dbConnect(){
-
-    try {
+        try {
             $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-        $dotenv->load();
+            $dotenv->load();
 
 
-                     $host = $_ENV['DB_HOST'];
-                     $user = $_ENV['DB_USER'];
-                     $pass = $_ENV['DB_PASS'];
-                     $dbname = $_ENV['DB_NAME'];
+                         $host = $_ENV['DB_HOST'];
+                         $user = $_ENV['DB_USER'];
+                         $pass = $_ENV['DB_PASS'];
+                         $dbname = $_ENV['DB_NAME'];
 
-                    $dsn = "mysql:host=${host};dbname=${dbname};charset=utf8mb4";
+                        $dsn = "mysql:host=${host};dbname=${dbname};charset=utf8mb4";
 
-                    $dbh = new PDO($dsn,$user,$pass,[PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>false,]);
-                    echo '接続成功';
+                        $dbh = new PDO($dsn,$user,$pass,[PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_EMULATE_PREPARES=>false,]);
 
 
-    } catch (PDOException $e) {
-            echo '接続失敗' . $e->getMessage() .  "\n";
-            exit();
+        } catch (PDOException $e) {
+                echo '接続失敗' . $e->getMessage() .  "\n";
+                exit();
+        }
+        return $dbh;
     }
-    return $dbh;
-}
+
+
+
+
+
 
 
 
